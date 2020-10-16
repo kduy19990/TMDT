0980a14dbfcde5e96e8ccd5993f186822b78650b
sinhvien <sinhvien>
2020-10-16 10:29:24 +0700

    new

---
+++ Tuan2/Tuan2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tuan2.Startup))]
+namespace Tuan2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
